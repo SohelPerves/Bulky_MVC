7dc2df88306acf188b30e502fa072c64c23d98a4
Sohel <srperves30@gmail.com>
2025-01-24 02:46:50 +0600

    Initial Commit

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using BulkyWeb.Models;
+using BulkyWeb.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+
+        return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name", "The DisplayOrder can't math exactly match name!");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFormDb = _db.Categories.Find(id);//its only working with id
+            /*Category? categoryFormDb1 = _db.Categories.FirstOrDefault(x=>x.Id==id); // here we can use name and other fields
+            Category? categoryFormDb2 = _db.Categories.Where(x=>x.Id==id).FirstOrDefault(); //Used for more filteration*/
+            if (categoryFormDb == null) 
+            {
+                return NotFound();
+            }
+            return View(categoryFormDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        //Deleting
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFormDb = _db.Categories.Find(id);//its only working with id
+            /*Category? categoryFormDb1 = _db.Categories.FirstOrDefault(x=>x.Id==id); // here we can use name and other fields
+            Category? categoryFormDb2 = _db.Categories.Where(x=>x.Id==id).FirstOrDefault(); //Used for more filteration*/
+            if (categoryFormDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFormDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ BulkyWeb/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ): base( options ) 
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        //Seeding
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // overring in behaviour
+        {
+            modelBuilder.Entity<Category>().HasData
+                (
+                    new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                    new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                    new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
